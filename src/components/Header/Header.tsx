49c3ed7f2c1c53c0d5edd62939b6e7a8c2828cc4
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-04-19 10:19:59 -0400

    finished header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
+import {Link } from 'react-router-dom'
+import './Header.css'
 
 export interface IHeaderProps {};
 
 const Header: React.FunctionComponent<IHeaderProps> = props => {
   return (
-   <Header>
-    <h1>Header</h1>
-   </Header>
+    <Container className='header_container'>
+      <Row>
+      <Col lg='6' md='6' className='center'>
+          <section className="header_content">
+            <h2 className='header_title'>Dan's Hats</h2>
+            <p> Only the greatest of hats</p>
+            <div className='button_container'>
+              <button
+              className='buy_btn'>
+               Buy Now
+              </button>
+            </div>
+              
+             
+          </section>
+        </Col>
+
+      </Row>
+    </Container>
   )
 }
 
