8298dfca47db4878046de16c8be1afdd58418656
Marcia McDonald-Scott <marcia.mcdonaldscott@shopify.com>
2023-04-25 16:17:37 -0400

    Added button to featured product card to navigate to detail view

--- src/components/FeatureSection/FeatureCard.tsx
+++ src/components/FeatureSection/FeatureCard.tsx
@@ -1,45 +1,105 @@
-import React from 'react';
-import {AlphaCard, Text} from '@shopify/polaris';
-import {Button} from '@shopify/polaris';
+import React, { useState, useCallback }  from 'react';
+import { AlphaCard, Button, VerticalStack, Text, TextField } from '@shopify/polaris';
+import { Product } from '../../types/types';
+import { Link } from 'react-router-dom';
 
-export interface IFeatureCardProps {
-  name: string;
-  image: string;
-  price: number
-};
+// export interface IFeatureCardProps {
+//   name: string;
+//   image: string;
+//   price: number
+// };
 
-const FeatureCard: React.FunctionComponent<IFeatureCardProps> = ({name, image, price}) => {
+// const FeatureCard: React.FunctionComponent<IFeatureCardProps> = ({name, image, price}) => {
+//   return (
+//     <AlphaCard
+//     background="bg-subdued"
+//     padding={{xs: '4', sm: '5', md: '6', lg: '8', xl: '10'}}>
+//       <img
+//         alt=""
+//         width="100%"
+//         height="100%"
+//         style={{
+//           objectFit: 'cover',
+//           objectPosition: 'center',
+//         }}
+//         src={image}
+//       />
+//     <Text as="h2" variant="bodyMd" fontWeight='bold' alignment='center'>
+//       {name}
+//     </Text>
+//     <Text as="h2" variant="bodyMd" fontWeight='bold' alignment='center'>
+//       ${price}
+//     </Text>
+//     <section
+//     style={{
+//          marginTop: '15px',
+//           display: 'flex',
+//           justifyContent: 'center',
+//         }}>
+//       <Button primary>Add to Cart</Button>
+//     </section>
+
+//   </AlphaCard>
+//   )
+// }
+
+
+interface Props {
+  product: Product;
+  setProductDetail: (productId: number) => void;
+}
+
+const FeatureCard: React.FC<Props> = ({ product, setProductDetail }) => {
+  const [quantity, setQuantity] = useState('1');
+
+  const handleQuantityChange = useCallback(
+    (newValue: string) => setQuantity(newValue),
+    [],
+  );
+
+  const handleClick:any = () => {
+    setProductDetail(product.id)
+  }
+
   return (
-    <AlphaCard 
-    background="bg-subdued" 
+    <AlphaCard
+    background="bg-subdued"
     padding={{xs: '4', sm: '5', md: '6', lg: '8', xl: '10'}}>
       <img
-        alt=""
+        alt={product.name}
         width="100%"
         height="100%"
         style={{
           objectFit: 'cover',
           objectPosition: 'center',
         }}
-        src={image}
+        src={product.image}
       />
-    <Text as="h2" variant="bodyMd" fontWeight='bold' alignment='center'>
-      {name}
+    <Text as="h2" variant="headingMd" alignment='start'>
+    {product.name}
     </Text>
-    <Text as="h2" variant="bodyMd" fontWeight='bold' alignment='center'>
-      ${price}
+    <Text as="h2" variant="bodyLg" fontWeight='bold' alignment='center'>
+    ${product.price}
     </Text>
-    <section    
-    style={{
-         marginTop: '15px',
-          display: 'flex',
-          justifyContent: 'center',
-        }}>
-      <Button primary>Add to Cart</Button>
-    </section>
+    <VerticalStack gap="5">
+             <TextField
+              label="Quantity"
+              type="number"
+              value={quantity}
+              onChange={handleQuantityChange}
+              autoComplete="off"
+            />
+          <Button primary fullWidth>Add to Cart</Button>
+          <Link to= {`/products/${product.id}`}>
+           <Button
+          onClick={ handleClick }
+          plain>View Full Details</Button>
+          </Link>
 
-  </AlphaCard>
-  )
-}
+          </VerticalStack>
+
+    </AlphaCard>
+  );
+};
 
 export default FeatureCard
--- src/components/FeatureSection/FeatureSection.tsx
+++ src/components/FeatureSection/FeatureSection.tsx
@@ -1,34 +1,14 @@
 import React from 'react'
-import {VerticalStack, Box, Text, HorizontalStack} from '@shopify/polaris';
+import {VerticalStack, Box, HorizontalStack} from '@shopify/polaris';
 import FeatureCard from './FeatureCard';
+import { Product } from '../../types/types';
 
-export interface IFCProps {};
+interface Props {
+  featureProducts: Product[];
+  setProductDetail: (productId: number) => void;
+}
 
-const featureProducts = [
-  {
-    "id": 7,
-    "name": "Knit Hat",
-    "description": "A warm knit hat made of soft acrylic yarn.",
-    "price": 19.99,
-    "image": "https://images.unsplash.com/photo-1510598969022-c4c6c5d05769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmVhbmllJTIwaGF0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    "id": 2,
-    "name": "Beanie",
-    "description": "A cozy beanie made of soft acrylic yarn.",
-    "price": 19.99,
-    "image": "https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmVhbmllJTIwaGF0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-  },
-  {
-    "id": 9,
-    "name": "Bucket Hat",
-    "description": "A trendy bucket hat made of cotton twill.",
-    "price": 24.99,
-    "image": "https://images.unsplash.com/photo-1600074178688-c7a0a748a3e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJ1Y2tldCUyMGhhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-  },
-]
-
-const FeatureSection: React.FunctionComponent<IFCProps> = props => {
+  const FeatureSection: React.FC<Props> = ({ featureProducts, setProductDetail}) => {
   return (
     <VerticalStack align="center" gap="4">
       <h2
@@ -45,19 +25,10 @@ const FeatureSection: React.FunctionComponent<IFCProps> = props => {
         {featureProducts.map((product)=>{
           return  (
             <Box id="box1" padding="4" width="550px">
-              <FeatureCard name={product.name} image={product.image} price={product.price}/>
+              <FeatureCard product={product} setProductDetail={setProductDetail}/>
             </Box>
           )
         })}
-          
-      
-        
-          {/* <Box id="box2" padding="4" width="550px">
-            <FeatureCard />
-          </Box>
-          <Box id="box3"padding="4" width="550px">
-            <FeatureCard />
-          </Box> */}
         </HorizontalStack>
       </section>
     </VerticalStack>
@@ -66,4 +37,4 @@ const FeatureSection: React.FunctionComponent<IFCProps> = props => {
 
 
 
-export default FeatureSection
+export default FeatureSection;
